7275fdaba9a9e110f7b52ae0195b0d1b9134ee67
Daniel Uebelein <dan.uebelein@gmail.com>
2015-09-28 13:17:36 -0500

    email

--- app/mailers/piggybak/notifier.rb
+++ app/mailers/piggybak/notifier.rb
@@ -3,10 +3,12 @@ module Piggybak
     default :from => Piggybak.config.email_sender,
             :cc => Piggybak.config.order_cc
 
-    def order_notification(order)
+    def order_notification(order, email=nil)
       @order = order
-
-      mail(:to => order.email,
+      if email = nil
+        email = order.email
+      end
+      mail(:to => email,
            :subject => "Order Confirmation ##{sprintf '%06d', @order.id}")
     end
   end
