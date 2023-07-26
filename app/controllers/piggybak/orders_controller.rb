7c60100cee2f1718ba0e459ce66f5c11b916f5c1
SCMths <matheus@supplyclinic.com>
2023-07-26 11:01:03 -0400

    refact(change error addition): change how information from errors are handled

--- app/controllers/piggybak/orders_controller.rb
+++ app/controllers/piggybak/orders_controller.rb
@@ -56,7 +56,7 @@ module Piggybak
             logger.warn "#{request.remote_ip}:#{Time.now.strftime("%Y-%m-%d %H:%M")} Order exception: #{e.inspect}"
           end
           if @order.errors.empty?
-            @order.errors[:base] << "Your order could not go through. Please try again."
+            @order.errors[:base].add('Your order could not go through. Please try again.')
           end
         end
       else
