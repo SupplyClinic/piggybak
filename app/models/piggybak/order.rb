0c0ab71ffb5ac3bcbd325f18780ce615c1f78ec2
Jonas Castanheira <jonas@supplyclinic.com>
2025-04-18 16:48:46 -0300

    fix: use authorization status enum with positional arguments bump: version to 0.8

--- app/models/piggybak/order.rb
+++ app/models/piggybak/order.rb
@@ -1,6 +1,6 @@
 module Piggybak
   class Order < ActiveRecord::Base
-    enum authorization_status: { unauthorized: 0, partially_authorized: 1, fully_authorized: 2 }
+    enum :authorization_status, { unauthorized: 0, partially_authorized: 1, fully_authorized: 2 }
 
     has_many :line_items, :inverse_of => :order
     has_many :order_notes, :inverse_of => :order
